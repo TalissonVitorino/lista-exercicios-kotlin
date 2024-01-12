a6b2f9df21dd78365ff4e26d288547794643a4d2
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2024-01-12 11:48:36 -0300

    Refatoração do Ex 04 para uso de função.

--- src/lista/Ex04.kt
+++ src/lista/Ex04.kt
@@ -8,17 +8,20 @@ fun main() {
     val numero2 = readln().toInt()
     println("Digite um número: ")
     val numero3 = readln().toInt()
+    val resulatdoFun = maior(numero1, numero2, numero3)
+    println("O número maior e: $resulatdoFun")
+}
 
+fun maior(numero1: Int, numero2: Int, numero3: Int): Int {
     if (numero1 > numero2) {
         if (numero1 > numero3) {
-            println(numero1)
+            return numero1
         } else {
-            println(numero3)
+            return numero3
         }
     } else if (numero2 > numero3) {
-        println(numero2)
+        return numero2
     } else {
-        println(numero3)
+        return numero3
     }
-
 }
