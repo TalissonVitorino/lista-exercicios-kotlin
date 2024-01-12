0ebff0b9e8700abbfc43ecc31ceb80af5883dd41
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2024-01-12 11:37:31 -0300

    Refatoração do Ex 03 para uso de função.

--- src/lista/Ex03.kt
+++ src/lista/Ex03.kt
@@ -5,10 +5,13 @@ import java.time.LocalDate
 fun main() {
     print("Digite seu ano de nascimento: ")
     val anoNascimento = readln().toInt()
+    val idade = calculaIdade(anoNascimento)
+    println("Sua idade atual é: $idade")
+}
+
+fun calculaIdade(anoNascimento: Int): Int {
     val anoAtual = LocalDate.now().year
     val idade = anoAtual - anoNascimento
-    println("Sua idade atual é: $idade")
-
-
-
+    return idade
 }
+
