12c008a4942adf7d272590716c3eca61bfc5762a
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2024-01-12 12:53:21 -0300

    Refatoração do Ex 07 para uso de função.

--- src/lista/Ex07.kt
+++ src/lista/Ex07.kt
@@ -22,15 +22,19 @@ package lista
 fun main() {
     print("Digite o numerador: ")
     val numerador = readln().toDouble()
+    val denominador = lerDenominador()
+    val divisao = numerador / denominador
+    println(divisao)
+}
+
+fun lerDenominador(): Double {
     print("Digite o denominador: ")
     var denominador = readln().toDouble()
 
-    while (denominador == 0.0){
-       println("Número inválido para a divisão! ")
-       print("Digite o denominador: ")
-       denominador = readln().toDouble()
-   }
-    val divisao = numerador / denominador
-    println(divisao)
-
+    while (denominador == 0.0) {
+        println("Número inválido para a divisão! ")
+        print("Digite o denominador: ")
+        denominador = readln().toDouble()
+    }
+    return denominador
 }
